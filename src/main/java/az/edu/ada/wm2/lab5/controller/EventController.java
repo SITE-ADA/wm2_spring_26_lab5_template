5dd8260fa69237b2f16403c9b376349dc39ea213
nsadili <nuraddinsadili93@gmail.com>
2026-02-26 14:24:28 +0400

    Fallback to github_token

--- src/main/java/az/edu/ada/wm2/lab5/controller/EventController.java
+++ src/main/java/az/edu/ada/wm2/lab5/controller/EventController.java
@@ -3,13 +3,10 @@ package az.edu.ada.wm2.lab5.controller;
 import az.edu.ada.wm2.lab5.model.Event;
 import az.edu.ada.wm2.lab5.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
